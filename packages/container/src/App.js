62417f71376cd78b6ca3aa99b957322e34388d52
Shubham Bhatt <shubhatt1@WKMZTB8708D8.global.publicisgroupe.net>
2023-03-01 22:41:19 +0530

    fix1

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return (
     <div>
-      <h1>Hi From Container Hehe</h1>
+      <h1>Hi From Container Hehee</h1>
       <hr />
       <MarketingApp />
     </div>
